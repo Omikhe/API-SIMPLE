9cf4afc4d952b2c5394a6ef05782e7be79dde62f
Omikhe <msakamike84@gmail.com>
2024-09-08 23:32:04 +0200

    add status code

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ import express from 'express'
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send('Hello')
+app.get("/add", (req, res) => {
+    res.status(201); //CREATED
+    res.send('Hello');
 });
 
 app.listen(port, () => { console.log(`Hey, go to http://localhost:${port}`) });
