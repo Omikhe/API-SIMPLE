a55ab118aa5164bf12b88f906f19377ffe918f4f
Omikhe <msakamike84@gmail.com>
2024-09-08 23:40:13 +0200

    status codes with node package

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-import express from 'express'
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const app = express();
 const port = 3000;
 
-app.get("/add", (req, res) => {
-    res.status(201); //CREATED
-    res.send('Hello');
+app.get("/hello-world", (req, res) => {
+    res.status(StatusCodes.OK);
+    res.send('Hello World');
 });
 
 app.listen(port, () => { console.log(`Hey, go to http://localhost:${port}`) });
