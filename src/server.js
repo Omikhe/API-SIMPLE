dc5f2c1aaae47fa704a09ffcc48b061b0ab8f731
Omikhe <msakamike84@gmail.com>
2024-09-09 23:28:58 +0200

    explore POST and basic  validation

--- src/server.js
+++ src/server.js
@@ -4,9 +4,34 @@ import { StatusCodes } from 'http-status-codes';
 const app = express();
 const port = 3000;
 
+const STATUS = {
+    SUCCESS: 'OK',
+    FAILURE: 'NO'
+}
+
+app.use(express.json());
+
 app.get("/hello-world", (req, res) => {
     res.status(StatusCodes.OK);
     res.send('Hello World');
 });
+
+app.post("/add", (req, res) => {
+    const data = [];
+    const { body } = req;
+
+    if (!body.name) {
+        res.status(StatusCodes.BAD_REQUEST).send({
+            status: STATUS.FAILURE,
+            message: 'Name is required'
+        });
+    }
+
+
+    return res.status(StatusCodes.CREATED).send({
+        status: STATUS.SUCCESS,
+        message: body.name,
+    });
+})
 
 app.listen(port, () => { console.log(`Hey, go to http://localhost:${port}`) });
