d09cdf184f657e5976ccff3cd513526fbe5d8db0
Omikhe <msakamike84@gmail.com>
2024-09-11 18:20:57 +0200

    add dummy data

---
+++ src/models/data/users.data.js
@@ -0,0 +1,11 @@
+const users = [
+    {
+        id: 1,
+        name: 'Omikhe',
+        email: 'omikhe265@gmail.com',
+        city: 'Zomba',
+        country: 'Malawi'
+    }
+]
+
+export default users;
