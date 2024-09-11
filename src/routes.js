8bd94cf9c03beec265e49efdb71bf62b495f0761
Omikhe <msakamike84@gmail.com>
2024-09-11 23:54:23 +0200

    add/update endpoints

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,58 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import userService from './services/user.services'
+
 const router = express.Router();
-const port = 3000;
 
 const STATUS = {
-    status: 'OK',
+    success: 'OK',
     failure: 'NO'
 }
 
-router.get("/hello-world", (req, res) => {
+router.get("/ping", (req, res) => {
     res.status(StatusCodes.OK);
-    res.send('Hello World');
+    res.send('OK');
 });
 
 router.post("/add", (req, res) => {
-    const data = [];
-    const { body } = req;
+    // const data = [];
+    const { body: user } = req;
 
-    if (!body.name) {
-        res.status(StatusCodes.BAD_REQUEST).send({
-            status: STATUS.failure,
-            message: 'Name is required'
-        });
-    }
+    const addedUser = userService.addUser(user)
+
+    // if (!user.name) {
+    //     res.status(StatusCodes.BAD_REQUEST).send({
+    //         status: STATUS.failure,
+    //         message: 'Name is required'
+    //     });
+    // }
 
 
     return res.status(StatusCodes.CREATED).send({
-        status: STATUS.status,
-        message: body.name,
+        status: STATUS.success,
+        message: addedUser,
     });
+})
+
+router.put('/update/:id', (req, res) => {
+    const { body: user } = req;
+
+    const id = parseInt(req.params.id, 10)
+
+    const updatedUser = userService.updateUser(id, user);
+
+    if (updatedUser) {
+        return res.status(StatusCodes.OK).send({
+            status: STATUS.success,
+            message: addedUser,
+        });
+    } else {
+        return res.status(StatusCodes.OK).send({
+            status: STATUS.failure,
+            message: `User "${id}" is not found`,
+        });
+    }
 })
 
 export default router;
